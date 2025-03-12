22056945c5c1760b8d1c565e11700a1799cb94b8
leekwansoo <leekwansoo49@gmail.com>
2025-03-12 17:52:25 +0900

    Update app.py
    Call mainapp after api_key is inserted

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st 
 import os
-from mainapp import main
 from dotenv import load_dotenv
 load_dotenv()
 #os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
@@ -12,8 +11,8 @@ openai_api_key = st.sidebar.text_input("Enter Your OPENAI_API_KEY")
 if openai_api_key:
     os.environ["OPENAI_API_KEY"] = openai_api_key
     st.sidebar.write("반갑습니다, Welcome to SCL-HEALTH_WEBZINE")
-    from mainapp import main
     
+    from mainapp import main
     main()
 
 
