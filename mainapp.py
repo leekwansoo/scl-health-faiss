0d6f85259db19478733d19c03dfbae5ebff07927
leekwansoo <leekwansoo49@gmail.com>
2025-08-25 17:54:55 +0900

    Refactor PDF handling and update function names

--- mainapp.py
+++ mainapp.py
@@ -3,7 +3,7 @@ import io
 import os
 import json 
 from langchain_community.document_loaders import TextLoader
-from modules.pdf_reader import generate_question, parse_pdf, create_query_file, load_pdf, add_qa_file, check_query_exist
+from modules.pdf_reader import generate_question, generate_question_with_genai, parse_pdf, create_query_file, load_pdf, add_qa_file, check_qafile_exist
 from modules.faissdb import store_pdf_documents
 from modules.query_handler import query_faiss_db
 from doc_handler import check_file_exist
@@ -16,6 +16,8 @@ st.session_state["DOCUMENT"] = []
 st.session_state["DOCUMENT"] = os.listdir("uploaded")
 
 doc_list =[st.session_state["DOCUMENT"]]
+
+st.session_state["query_input"] = [""]
     
 # Function to add data to DOCUMENT directory 
 def check_document(value): 
@@ -26,7 +28,9 @@ def check_document(value):
         result = "exist"
         return(result)
        
-def add_document(value):         
+def add_document(value):   
+    if "DOCUMENT" not in st.session_state: 
+        st.session_state["DOCUMENT"] = []     
     st.session_state["DOCUMENT"].append(value)
     st.write(f"Document added: {value}") 
 
@@ -41,7 +45,7 @@ def list_documents():
 
 
 def handle_query(file_name, query):
-    response =check_query_exist(file_name, query)
+    response = check_qafile_exist(file_name, query)
     if response:
         st.write(response["answer"])
         st.write("Get Answer from Home Brewed QA store")
@@ -78,25 +82,28 @@ def main():
                 text = parse_pdf(uploaded_file)
                 documents = load_pdf(uploaded_name)
                 #result = load_pdf_documents(documents)  # load documents to chromadb
-                result = store_pdf_documents(documents)  # load documents to faissdb
+                result = store_pdf_documents(documents)  # load documents to faiss_db
                 if result:
                     st.sidebar.write(result)
                     add_document(file_name)     
                 else: st.sidebar.write("storing PDF file into vector store failed")
-                questions= generate_question(text)
-                query_file = create_query_file(file_name, questions.content)
+                questions= generate_question_with_genai(text)
+                print(questions)
+                query_file = create_query_file(file_name, questions)
+                # questions= generate_question(text)
+                # query_file = create_query_file(file_name, questions.content)
                 st.session_state["query_file"].append(query_file)
-                for question in questions.content:
+                for question in questions:
                     st.session_state["query_message"].append(question)
-                st.sidebar.markdown(questions.content)
+                st.sidebar.markdown(questions)
                         
                 docs = list_documents()
                 if docs:
                     for doc in docs:
                         st.sidebar.write(f"Uploaded_Document: {doc}\n")  
             else:
-                st.sidebar.write(f"{file_name} is aleardy uploaded\n")     
-            
+                st.sidebar.write(f"{file_name} is already uploaded\n")     
+
         else:
             st.sidebar.write("Please upload a PDF and select subject to get started.")
             
@@ -104,26 +111,32 @@ def main():
     elif options == "Query from Uploaded File":
         st.header("Query from Uploaded File")
         query_file_list = os.listdir("query")
-        selected = st.sidebar.selectbox("Select month to query", query_file_list)
+        selected = st.sidebar.selectbox("Select document to query", query_file_list)
         file_name = f"query/{selected}"
         # make query as list in the query_file
         loader =TextLoader(file_name, encoding = "utf-8")
         documents = loader.load()
         query_list = documents[0].page_content.split("\n")
-        query_input = st.text_input("Enter your question for your uploaded documents")
+        query_input = st.text_input("Enter your question for your uploaded documents:", 
+                                    key = "query_key_0",
+                                    value = st.session_state["query_input"][0])
         if query_input:
-            handle_query(file_name, query_input)
-            
+            if st.button("Get Answer"):
+                handle_query(file_name, query_input)
+
+        #st.session_state["query_input"] = [""]
         i = 0
         for query in query_list:
-            i += 1           
-            st.sidebar.write(query)  # Display the query
-            if "?" in query:
+            i += 1
+            query = query.strip().rstrip(',')
+            if query.startswith('{') and query.endswith('}'):
+                query_dict = json.loads(query)
+                st.sidebar.write(query_dict["question"])
                 button = st.sidebar.button(f"Query", key=f"button_{i}")
-                if button:# Add a button with a unique key
-                    handle_query(file_name, query)
-            
-            
+                if button:
+                    st.session_state["query_input"][0] = query_dict["question"]
+                    st.rerun()
+
     elif options == "Web Search":
         st.header("Web Search")
         query = st.text_input("Enter a search query:")
